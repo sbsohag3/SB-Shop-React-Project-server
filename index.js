91d8ec362a844c3486c42008c466aaa0d6897ec2
sbsohag3 <sbsohag3@gmail.com>
2022-10-24 00:21:56 +0600

    updade

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "https://sb-shop-ass12.web.app" }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sbshop.szt3m.mongodb.net/?retryWrites=true&w=majority`;
